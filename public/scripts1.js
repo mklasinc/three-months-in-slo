c22c315681a359ab41d52ccaaec3e54586670f4a
Miha Klasinc <Miha Klasinc>
2015-12-14 15:53:35 +0400

    ljeft

--- public/scripts1.js
+++ public/scripts1.js
@@ -87,7 +87,7 @@ var group4 = svg.append("g");
 var projection = d3.geo.mercator()
 	.scale(svgWidth*10)
 	.translate([svgWidth/2, svgHeight/2])
-	.center([14.60, 46.1]);
+	.center([14.70, 46.1]);
 //PATH
 var path = d3.geo.path()
 	.projection(projection);
